8a36b36293e44367fb09254b88785a89a54933be
David Arce <david@zellship.com>
2022-10-21 23:45:50 -0500

    insall taildwind

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import HomePage from './views/Home/HomePage';
-
+import './css/app.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
